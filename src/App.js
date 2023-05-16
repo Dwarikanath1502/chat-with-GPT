1d99b338014eb41b8f7ca5e771e748baa36d0ecf
Dwarikanath1502 <ddwarikanath15@gmail.com>
2023-05-16 21:01:57 +0530

    created chat app with chat gpt API is not getting fetched, screen is responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
 
-function App() {
+const API_KEY = "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"
+
+
+const App = () => {
+
+  const [messages, setMessages] = useState([
+    {
+      message: "I am developed by Dwarikanath. Ask me anything I'll happy to help",
+      sender: "ChatGPT"
+    }
+  ])
+  const [typing, setTyping] = useState(false)
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages, newMessage] //all old msg + new msg
+
+    // update message state
+    setMessages(newMessages);
+
+    // set typing indicator
+    setTyping(true);
+
+  // process message tof chatGPT
+    await processMessageToChatGPT(newMessages);
+
+
+  }
+
+ async function  processMessageToChatGPT(chatMessages) {
+    //  chatMessages {sender: "user" or "chatGPT", messaege: "The message content here"}
+    // apiMessages{ role: "user" or "assistant", content: "The message is here"}
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant"
+      } else {
+        role = "user"
+      }
+      return { role: role, content: messageObject.message }
+    })
+
+    // role : "user" -> msg from user, "assistant"-> response from chatGPT
+    // "system"-> generally one initial message defining how we want to talk to chatGPT
+
+    const systemMessage = {
+      role: "system",
+      content: "Expalin all concepts like I am 10 year old."
+    }
+
+
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,
+        ...apiMessages //[this is our msg fom chat with chatGPT
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer" + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      // console.log(data.choices[0].message.content);
+      setMessages(
+        [...chatMessages, {
+          message: data.choices[0].message.content,
+          sender:  "ChatGPT"
+        }]
+      )
+      setTyping(false);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div style={{ position: 'relative', height: '100vh' }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              typingIndicator={typing ? <TypingIndicator content='Robot is typing...' /> : null}
+              scrollBehavior='smooth'
+            >
+              {messages.map((message, index) => {
+                return <Message key={index} model={message} />
+              })}
+            </MessageList>
+            <MessageInput
+              placeholder='Type here...'
+              onSend={handleSend}
+            />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
